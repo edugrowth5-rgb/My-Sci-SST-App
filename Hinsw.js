c2fdb005977b28dca770f74512174bbfb1995140
edugrowth5-rgb <edugrowth5@gmail.com>
2026-05-24 10:42:05 +0530

    Add service worker for caching assets

---
+++ Hinsw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'success-zone-v1';
+// List all files with the QP prefix to be cached
+const urlsToCache = [
+  './',
+  './index.html',
+  './HinSci.css',
+  './HinSci.js',
+  './HinSciCh1.js',
+  './HinSciCh2.js',
+  './HinSciCh3.js',
+  './HinSciCh4.js',
+  './HinSciCh5.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
