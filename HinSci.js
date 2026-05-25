741ba265c194f23a006d3bb42e3e9dbeafcf7db5
edugrowth5-rgb <edugrowth5@gmail.com>
2026-05-25 18:40:13 +0530

    Add case for quizData assignment for num === 7

--- HinSci.js
+++ HinSci.js
@@ -43,6 +43,7 @@ function loadChapter(num) {
     else if(num === 4) { quizData = HinSciCh4Data; }
     else if(num === 5) { quizData = typeof HinGeoCh1Data !== 'undefined' ? HinGeoCh1Data : []; }
     else if(num === 6) { quizData = typeof HinGeoCh2Data !== 'undefined' ? HinGeoCh2Data : []; }
+    else if(num === 7) { quizData = typeof HinGeoCh3Data !== 'undefined' ? HinGeoCh3Data : []; }
     
     curIdx = 0;
     score = 0;
